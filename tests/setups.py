bfedda07d8012fee92f52a9bbde83b99da384c8d
RSchmitzHH <r.schmitz@uke.de>
2020-11-16 18:17:25 +0100

    As fixtures

--- tests/setups.py
+++ tests/setups.py
@@ -1,4 +1,4 @@
-
+import pytest
 import numpy as np
 
 valid_measures = [
@@ -8,9 +8,15 @@ valid_measures = [
 
 invalid_string_input = 'rghrjgrdgdgsacvxcvysavbbzfbfd'
 
-dummy_predictions = np.random.random((32, 1, 128, 128)).astype(np.float32)
-dummy_predictions[:, :, 48:80, 48:80] = 0.8
 
-dummy_targets = np.ones_like(dummy_predictions)
+@pytest.fixture
+def dummy_predictions():
+    dummy_predictions = np.random.random((32, 1, 128, 128)).astype(np.float32)
+    dummy_predictions[:, :, 48:80, 48:80] = 0.8
+    return dummy_predictions
 
 
+@pytest.fixture
+def dummy_targets():
+    dummy_targets = np.ones_like(dummy_predictions)
+    return dummy_targets
