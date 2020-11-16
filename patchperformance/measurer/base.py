c2874227cd6b2921faf1a9e325bdba46e36dafde
Frederic Madesta <frederic.madesta@gmail.com>
2020-11-16 19:28:42 +0100

    raise error in abstract static methods

--- patchperformance/measurer/base.py
+++ patchperformance/measurer/base.py
@@ -5,14 +5,24 @@ class BaseMeasurer(ABC):
     @staticmethod
     @abstractmethod
     def sum_batch(batch_tensor):
-        pass
+        raise NotImplementedError
 
     @staticmethod
     @abstractmethod
     def binary_cross_entropy(prediction, target):
-        pass
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def binary_cross_entropy_with_logits(prediction, target):
+        raise NotImplementedError
 
     @staticmethod
     @abstractmethod
     def l2(prediction, target):
-        pass
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def l2_with_logits(prediction, target):
+        raise NotImplementedError
