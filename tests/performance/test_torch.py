351b13cd7c80d2bc72f4f7c62af689d2625ca50d
Frederic Madesta <frederic.madesta@gmail.com>
2020-11-19 12:41:55 +0100

    added simple training test for pytorch

--- tests/performance/test_torch.py
+++ tests/performance/test_torch.py
@@ -72,3 +72,36 @@ class TestTorchPatchPerformance:
         assert torch.allclose(forwarded_loss_value, direct_loss_value)
         assert patch_performance.n_patches_seen == dummy_predictions.shape[0]
         assert patch_performance._measure_sum.shape == dummy_predictions.shape[1:]
+
+    @pytest.mark.parametrize('measure', valid_measures)
+    @pytest.mark.parametrize('loss', torch_losses)
+    def test_simple_training(
+            self,
+            measure,
+            loss,
+            dummy_torch_targets
+    ):
+        dummy_torch_targets = torch.from_numpy(dummy_torch_targets)
+        model = nn.Sequential(
+            nn.Conv2d(1, 1, (3, 3), padding=1),
+            nn.Sigmoid()
+        )
+
+        patch_performance = TorchPatchPerformance(
+            loss=loss,
+            measure=measure
+        )
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+        model.train(True)
+        for epoch in range(10):
+            optimizer.zero_grad()
+            dummy_output = model(dummy_torch_targets)
+            loss_value = patch_performance(dummy_output, dummy_torch_targets)
+            loss_value.backward()
+
+            patch_performance.calculate_performance().cpu().numpy()
+            patch_performance.reset()
+
+            assert patch_performance.n_patches_seen == 0
+            assert patch_performance._measure_sum is None
